b2bf0636b59ba2e52fa8fac320138657d0ddf6f0
CliveDev0413 <anoodleofcode@gmail.com>
2021-05-16 14:27:36 +0800

    added the vs her mod

---
+++ source_her/DialogueBox.hx
@@ -0,0 +1,336 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.text.FlxTypeText;
+import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.group.FlxSpriteGroup;
+import flixel.input.FlxKeyManager;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
+
+using StringTools;
+
+class DialogueBox extends FlxSpriteGroup
+{
+	var box:FlxSprite;
+
+	var curCharacter:String = '';
+
+	var dialogue:Alphabet;
+	var dialogueList:Array<String> = [];
+
+	// SECOND DIALOGUE FOR THE PIXEL SHIT INSTEAD???
+	var swagDialogue:FlxTypeText;
+
+	var dropText:FlxText;
+
+	public var finishThing:Void->Void;
+
+	var senpai:FlxSprite;
+	var bfPixel:FlxSprite;
+	var her:FlxSprite;
+	var bf:FlxSprite;
+
+	var handSelect:FlxSprite;
+	var bgFade:FlxSprite;
+
+	public function new(talkingRight:Bool = true, ?dialogueList:Array<String>)
+	{
+		super();
+
+		switch (PlayState.SONG.song.toLowerCase())
+		{
+			case 'senpai':
+				FlxG.sound.playMusic(Paths.music('Lunchbox'), 0);
+				FlxG.sound.music.fadeIn(1, 0, 0.8);
+			case 'thorns':
+				FlxG.sound.playMusic(Paths.music('LunchboxScary'), 0);
+				FlxG.sound.music.fadeIn(1, 0, 0.8);
+			case 'neurosurgery':
+				FlxG.sound.playMusic(Paths.music('demo_weeknd_sampl'), 0);
+				FlxG.sound.music.fadeIn(1, 0, 0.8);
+			case 'prognostics':
+				FlxG.sound.playMusic(Paths.music('demo_weeknd_sampl'), 0);
+				FlxG.sound.music.fadeIn(1, 0, 0.8);
+		}
+
+		bgFade = new FlxSprite(-200, -200).makeGraphic(Std.int(FlxG.width * 1.3), Std.int(FlxG.height * 1.3), 0xFFB3DFd8);
+		bgFade.scrollFactor.set();
+		bgFade.alpha = 0;
+		add(bgFade);
+
+		new FlxTimer().start(0.83, function(tmr:FlxTimer)
+		{
+			bgFade.alpha += (1 / 5) * 0.7;
+			if (bgFade.alpha > 0.7)
+				bgFade.alpha = 0.7;
+		}, 5);
+
+		if(PlayState.SONG.song.toLowerCase() == 'neurosurgery' || PlayState.SONG.song.toLowerCase() == 'prognostics' || PlayState.SONG.song.toLowerCase() == 'neurodivergent')
+		{
+			box = new FlxSprite(-20, 380);
+		}
+		else
+		{
+			box = new FlxSprite(-20, 45);
+		}
+
+		var hasDialog = false;
+		switch (PlayState.SONG.song.toLowerCase())
+		{
+			case 'senpai':
+				hasDialog = true;
+				box.frames = Paths.getSparrowAtlas('weeb/pixelUI/dialogueBox-pixel');
+				box.animation.addByPrefix('normalOpen', 'Text Box Appear', 24, false);
+				box.animation.addByIndices('normal', 'Text Box Appear', [4], "", 24);
+			case 'roses':
+				hasDialog = true;
+				FlxG.sound.play(Paths.sound('ANGRY_TEXT_BOX'));
+
+				box.frames = Paths.getSparrowAtlas('weeb/pixelUI/dialogueBox-senpaiMad');
+				box.animation.addByPrefix('normalOpen', 'SENPAI ANGRY IMPACT SPEECH', 24, false);
+				box.animation.addByIndices('normal', 'SENPAI ANGRY IMPACT SPEECH', [4], "", 24);
+
+			case 'thorns':
+				hasDialog = true;
+				box.frames = Paths.getSparrowAtlas('weeb/pixelUI/dialogueBox-evil');
+				box.animation.addByPrefix('normalOpen', 'Spirit Textbox spawn', 24, false);
+				box.animation.addByIndices('normal', 'Spirit Textbox spawn', [11], "", 24);
+
+				var face:FlxSprite = new FlxSprite(320, 170).loadGraphic(Paths.image('weeb/spiritFaceForward'));
+				face.setGraphicSize(Std.int(face.width * 6));
+				add(face);
+			case 'neurosurgery' | 'prognostics' | 'neurodivergent':
+				hasDialog = true;
+				box.frames = Paths.getSparrowAtlas('herUI/speech_bubble_talking');
+				box.animation.addByPrefix('normalOpen', 'Speech Bubble Normal Open', 24, false);
+				box.animation.addByIndices('normal', 'speech bubble normal', [4], "", 24);
+
+		}
+
+		this.dialogueList = dialogueList;
+		
+		if (!hasDialog)
+			return;
+
+		senpai = new FlxSprite(-20, 40);
+		senpai.frames = Paths.getSparrowAtlas('weeb/senpaiPortrait');
+		senpai.animation.addByPrefix('enter', 'Senpai Portrait Enter', 24, false);
+		senpai.setGraphicSize(Std.int(senpai.width * PlayState.daPixelZoom * 0.9));
+		senpai.updateHitbox();
+		senpai.scrollFactor.set();
+		add(senpai);
+		senpai.visible = false;
+	
+		bfPixel = new FlxSprite(0, 40);
+		bfPixel.frames = Paths.getSparrowAtlas('weeb/bfPortrait');
+		bfPixel.animation.addByPrefix('enter', 'Boyfriend portrait enter', 24, false);
+		bfPixel.setGraphicSize(Std.int(bfPixel.width * PlayState.daPixelZoom * 0.9));
+		bfPixel.updateHitbox();
+		bfPixel.scrollFactor.set();
+		add(bfPixel);
+		bfPixel.visible = false;
+
+		if(PlayState.SONG.song.toLowerCase() == 'neurosurgery' || PlayState.SONG.song.toLowerCase() == 'prognostics' || PlayState.SONG.song.toLowerCase() == 'neurodivergent')
+		{
+			her = new FlxSprite(0, 150);
+			her.frames = Paths.getSparrowAtlas('herUI/herPortrait');
+			her.animation.addByPrefix('enter', 'her portrait enter', 24, false);
+			her.setGraphicSize(Std.int(her.width * 0.9));
+			her.updateHitbox();
+			her.scrollFactor.set();
+			add(her);
+			her.visible = false;
+		}
+	
+		bf = new FlxSprite(700, 210);
+		bf.frames = Paths.getSparrowAtlas('herUI/bfPortrait');
+		bf.animation.addByPrefix('enter', 'bf portrait enter', 24, false);
+		bf.setGraphicSize(Std.int(bf.width * 0.9));
+		bf.updateHitbox();
+		bf.scrollFactor.set();
+		add(bf);
+		bf.visible = false;
+		
+		box.animation.play('normalOpen');
+		if(PlayState.SONG.song.toLowerCase() == 'neurosurgery' || PlayState.SONG.song.toLowerCase() == 'prognostics' || PlayState.SONG.song.toLowerCase() == 'neurodivergent')
+		{
+			box.setGraphicSize(Std.int(box.width * 0.9));
+		}
+		else
+		{
+			box.setGraphicSize(Std.int(box.width * PlayState.daPixelZoom * 0.9));
+		}
+
+		box.updateHitbox();
+		add(box);
+
+		box.screenCenter(X);
+		//senpai.screenCenter(X);
+
+		handSelect = new FlxSprite(FlxG.width * 0.9, FlxG.height * 0.9).loadGraphic(Paths.image('weeb/pixelUI/hand_textbox'));
+		add(handSelect);
+
+
+		if (!talkingRight)
+		{
+			// box.flipX = true;
+		}
+
+		dropText = new FlxText(242, 502, Std.int(FlxG.width * 0.6), "", 32);
+		dropText.font = 'Pixel Arial 11 Bold';
+		dropText.color = 0xFFD89494;
+		add(dropText);
+
+		swagDialogue = new FlxTypeText(240, 500, Std.int(FlxG.width * 0.6), "", 32);
+		swagDialogue.font = 'Pixel Arial 11 Bold';
+		swagDialogue.color = 0xFF3F2021;
+		swagDialogue.sounds = [FlxG.sound.load(Paths.sound('pixelText'), 0.6)];
+		add(swagDialogue);
+
+		dialogue = new Alphabet(0, 80, "", false, true);
+		// dialogue.x = 90;
+		// add(dialogue);
+	}
+
+	var dialogueOpened:Bool = false;
+	var dialogueStarted:Bool = false;
+
+	override function update(elapsed:Float)
+	{
+		// HARD CODING CUZ IM STUPDI
+		if (PlayState.SONG.song.toLowerCase() == 'roses')
+			senpai.visible = false;
+		if (PlayState.SONG.song.toLowerCase() == 'thorns')
+		{
+			senpai.color = FlxColor.BLACK;
+			swagDialogue.color = FlxColor.WHITE;
+			dropText.color = FlxColor.BLACK;
+		}
+
+		dropText.text = swagDialogue.text;
+
+		if (box.animation.curAnim != null)
+		{
+			if (box.animation.curAnim.name == 'normalOpen' && box.animation.curAnim.finished)
+			{
+				box.animation.play('normal');
+				dialogueOpened = true;
+			}
+		}
+
+		if (dialogueOpened && !dialogueStarted)
+		{
+			startDialogue();
+			dialogueStarted = true;
+		}
+
+		if (FlxG.keys.justPressed.ANY  && dialogueStarted == true)
+		{
+			remove(dialogue);
+				
+			FlxG.sound.play(Paths.sound('clickText'), 0.8);
+
+			if (dialogueList[1] == null && dialogueList[0] != null)
+			{
+				if (!isEnding)
+				{
+					isEnding = true;
+
+					if (PlayState.SONG.song.toLowerCase() == 'senpai' || PlayState.SONG.song.toLowerCase() == 'thorns' || PlayState.SONG.song.toLowerCase() == 'neurosurgery' || PlayState.SONG.song.toLowerCase() == 'prognostics')
+						FlxG.sound.music.fadeOut(2.2, 0);
+
+					new FlxTimer().start(0.2, function(tmr:FlxTimer)
+					{
+						box.alpha -= 1 / 5;
+						bgFade.alpha -= 1 / 5 * 0.7;
+
+						if(PlayState.SONG.song.toLowerCase() != 'neurosurgery' || PlayState.SONG.song.toLowerCase() != 'prognostics' ||PlayState.SONG.song.toLowerCase() != 'neurodivergent')
+						{
+							senpai.visible = false;
+							bfPixel.visible = false;
+						}
+						else
+						{
+							her.visible = false;
+							bf.visible = false;
+						}
+
+						swagDialogue.alpha -= 1 / 5;
+						dropText.alpha = swagDialogue.alpha;
+					}, 5);
+
+					new FlxTimer().start(1.2, function(tmr:FlxTimer)
+					{
+						finishThing();
+						kill();
+					});
+				}
+			}
+			else
+			{
+				dialogueList.remove(dialogueList[0]);
+				startDialogue();
+			}
+		}
+		
+		super.update(elapsed);
+	}
+
+	var isEnding:Bool = false;
+
+	function startDialogue():Void
+	{
+		cleanDialog();
+		// var theDialog:Alphabet = new Alphabet(0, 70, dialogueList[0], false, true);
+		// dialogue = theDialog;
+		// add(theDialog);
+
+		// swagDialogue.text = ;
+		swagDialogue.resetText(dialogueList[0]);
+		swagDialogue.start(0.04, true);
+
+		switch (curCharacter)
+		{
+			case 'senpai':
+				bfPixel.visible = false;
+				if (!senpai.visible)
+				{
+					senpai.visible = true;
+					senpai.animation.play('enter');
+				}
+			case 'bf-pixel':
+				senpai.visible = false;
+				if (!bfPixel.visible)
+				{
+					bfPixel.visible = true;
+					bfPixel.animation.play('enter');
+				}
+			case 'her':
+				bf.visible = false;
+				if (!her.visible)
+				{
+					box.flipX = true;
+					her.visible = true;
+					her.animation.play('enter');
+				}
+			case 'bf':
+				her.visible = false;
+				if (!bf.visible)
+				{
+					box.flipX = false;
+					bf.visible = true;
+					bf.animation.play('enter');
+				}
+		}
+	}
+
+	function cleanDialog():Void
+	{
+		var splitName:Array<String> = dialogueList[0].split(":");
+		curCharacter = splitName[1];
+		dialogueList[0] = dialogueList[0].substr(splitName[1].length + 2).trim();
+	}
+}
